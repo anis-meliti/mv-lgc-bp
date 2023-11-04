f4a6ccb0722099cead3abcc522ef643c2a83556b
anis-meliti <ansmelliti@gmail.com>
2023-11-05 00:21:51 +0100

    create the boilerplate

--- .bin/install.mjs
+++ .bin/install.mjs
@@ -4,7 +4,7 @@ import cp from "child_process";
 import path from "path";
 import fs, { existsSync, mkdirSync } from "fs";
 // cli spinners
-import ora from "ora";
+// import ora from "ora";
 
 // convert libs to promises
 const exec = promisify(cp.exec);
@@ -33,12 +33,12 @@ else {
 }
 
 try {
-    const gitSpinner = ora("Downloading files...").start();
+    // const gitSpinner = ora("Downloading files...").start();
     // clone the repo into the project folder -> creates the new boilerplate
     await exec(`git clone --depth 1 ${git_repo} ${projectPath} --quiet`);
-    gitSpinner.succeed();
+    // gitSpinner.succeed();
 
-    const cleanSpinner = ora("Removing useless files").start();
+    // const cleanSpinner = ora("Removing useless files").start();
     // remove my git history
     const rmGit = rm(path.join(projectPath, ".git"), { recursive: true, force: true });
     // remove the installation file
@@ -48,11 +48,11 @@ try {
     process.chdir(projectPath);
     // remove the packages needed for cli
     await exec("npm uninstall ora cli-spinners");
-    cleanSpinner.succeed();
+    // cleanSpinner.succeed();
 
-    const npmSpinner = ora("Installing dependencies...").start();
+    // const npmSpinner = ora("Installing dependencies...").start();
     await exec("npm install");
-    npmSpinner.succeed();
+    // npmSpinner.succeed();
 
     console.log("The installation is done!");
     console.log("You can now run your app with:");
